60eda039bc967998b592f8c91830337b451dce6c
Krisandra <143172427+Krisandr-a@users.noreply.github.com>
2025-09-15 16:16:24 +0200

    fix: removed test comment from back

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BobappApplication {
-//	test
     public static void main(String[] args) {
 		SpringApplication.run(BobappApplication.class, args);
 	}
