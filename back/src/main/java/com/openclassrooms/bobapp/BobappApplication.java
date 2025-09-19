6089d02aca01731c6bb01ec276f6ffe17f586316
krisandra <krisandra.adams@gmail.com>
2025-09-19 12:27:03 +0200

    fix: comment to test workflows

--- back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
+++ back/src/main/java/com/openclassrooms/bobapp/BobappApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BobappApplication {
+    //test
     public static void main(String[] args) {
 		SpringApplication.run(BobappApplication.class, args);
 	}
